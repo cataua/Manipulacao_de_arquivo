b4f6046f2ecadb7e31755ceac7bf6615745b5ff6
Rogério Caetano <cataua+github@gmail.com>
2021-11-15 17:14:11 -0300

    Tratamento do vetor ch

--- main.cpp
+++ main.cpp
@@ -1,35 +1,38 @@
 
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
 #define MAX 10
 
-void main() {
+int main() {
     //char ch1,ch[MAX], vet[100] = {"/home/lourence/Documents/Sistemas/AlgoritmosII/numeros.txt"};
-    char content, ch1[4], ch[MAX][4], vet[100] = {"../file.txt"};
+    char ch[MAX], vet[100] = {"../file.txt"};
+    char line[4];
     int x = 0;
     FILE *fp;
 
-    fp = fopen(vet, "r"); // read mode
+    errno_t  err = fopen_s(&fp, vet, "r"); // read mode
 
-    if (fp == NULL)
+    if (err)
     {
         perror("Error while opening the file.\n");
         exit(EXIT_FAILURE);
     }
 
     printf("The contents of %s file are:\n", vet);
-
-    while(fgets(ch1, sizeof(ch1), fp))
+    line[4] = fgetc(fp);
+    int t = 0;
+    while(line[4] != EOF)
     {
-        if (ch1[4] == '\n') {
-            continue;
-        }
+        while (line[4] != '\n') {
+
 //        for(int x=0; x<MAX; x++)
 //        {
-            ch[x][4] = ch1[4];
-            x++;
+            ch[x] = (char)line[t];
+            t++;
+            line[4] = fgetc(fp);
 //        }
+        };
+            x++;
     }
 
     fclose(fp);
@@ -38,18 +41,18 @@ void main() {
     char aux;
 
 
-    for (i = 0; i < MAX ; i++) {
-        for(j = 0; j < MAX -1 ; j++) {
-            if(ch[j][4] > ch[j+1][4]) {
-                aux = ch[j][4];
-                ch[j][4] = ch[j+1][4];
-                ch[j+1][4] = aux;
-            }
-        }
-    }
-    for (i=0; i < MAX; i++){
-        printf("%4d,", ch[i]);
-    }
+//    for (i = 0; i < MAX ; i++) {
+//        for(j = 0; j < MAX -1 ; j++) {
+//            if(ch[j] > ch[j+1]) {
+//                aux = ch[j];
+//                ch[j] = ch[j+1];
+//                ch[j+1] = aux;
+//            }
+//        }
+//    }
+//    for (i=0; i < MAX; i++){
+//        printf("%c,", ch);
+//    }
 
     printf("\n\n");
 }
